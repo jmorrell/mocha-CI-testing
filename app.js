2ccc2539b86f791083a412d5c379ceb578800459
Jeremy Morrell <jmorrell@heroku.com>
2017-04-13 11:48:16 -0700

    Pivot the company

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('./index.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Cruel World!'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
